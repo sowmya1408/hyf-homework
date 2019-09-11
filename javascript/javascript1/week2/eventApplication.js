1fd80c7ad7f0cfa98f8d33061b9c86e2ca5f73a6
Sowmya Mannem <sowmyamannemr@gmail.com>
2019-09-11 17:36:14 +0200

    removed if else statement

--- javascript/javascript1/week2/eventApplication.js
+++ javascript/javascript1/week2/eventApplication.js
@@ -3,14 +3,11 @@ const getEventWeekDay = (eventDays) => {
     const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     const today = new Date();
     const totalDays = today.getDay() + eventDays;
-    if (totalDays <= 6){
-    return weekDays[totalDays];
-    } else {
-    const  totalDaysGreaterThanWeekDays = totalDays % weekDays.length; 
-    const eventWeekday = weekDays[totalDaysGreaterThanWeekDays];
+    const  dayIndex = totalDays % weekDays.length; 
+    const eventWeekday = weekDays[dayIndex];
     return eventWeekday;
  }
-}
+
 
 console.log(getEventWeekDay(9));
 console.log(getEventWeekDay(3));
