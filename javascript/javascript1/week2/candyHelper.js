f1f4228fc4e062bd15c482e683031d1c8d7b0044
Sowmya Mannem <sowmyamannemr@gmail.com>
2019-09-12 17:00:48 +0200

    added a for loop within function

--- javascript/javascript1/week2/candyHelper.js
+++ javascript/javascript1/week2/candyHelper.js
@@ -1,36 +1,45 @@
 const boughtCandyPrices = [];
 
 const addCandy = (candyType, weight) => {
-    if (candyType === 'sweet') {
+    switch(candyType) {
+        case 'sweet':
         return boughtCandyPrices.push(0.5 * weight);
-    } else if (candyType === 'Chocolate') {
+        break;
+        case 'Chocolate': 
         return boughtCandyPrices.push(0.7 * weight);
-    } else if (candyType === 'Toffee') {
+        break;
+        case 'Toffee': 
         return boughtCandyPrices.push(1.1 * weight);
-    } else if (candyType === 'Chewing-gum') {
+        break;
+        case 'Chewing-gum':
         return boughtCandyPrices.push(0.03 * weight);
-    } else { 
+        break;
+        default: 
         return "please enter candyType";
     }
 
 }
-// addCandy('Chewing-gum', 100);
+addCandy('Chewing-gum', 600);
 addCandy('Toffee', 30);
-// addCandy('Chocolate', 50);
-// addCandy('sweet', 60);
+addCandy('Chocolate', 50);
+addCandy('sweet', 60);
 console.log(boughtCandyPrices);
 
-const amountToSpend = Math.random() * 100;
-for (i = 0; i < boughtCandyPrices.length; i++) {
-    function canBuyMoreCandy() {
-        if (boughtCandyPrices[0] <= amountToSpend) {
+
+const canBuyMoreCandy = boughtCandyPrices =>  {
+    const amountToSpend = Math.random() * 100;
+    console.log(amountToSpend);
+
+         let sumPrice = 0;             
+        for (let i = 0; i < boughtCandyPrices.length; i++) {
+            sumPrice+= boughtCandyPrices[i];
+        }
+    console.log(sumPrice);
+          if (sumPrice <= amountToSpend) {
             return "You can buy more, so please do!";
         } else {
             return "Enough candy for you";
         }
-    }
+    } 
 
-}
-console.log(amountToSpend);
-const candy = canBuyMoreCandy();
-console.log(candy);
+console.log(canBuyMoreCandy(boughtCandyPrices));
