f1d786bfa52eef03097fb31aa70f1025ebcf1a53
Sowmya Mannem <sowmyamannemr@gmail.com>
2019-10-12 17:08:31 +0200

    removed the newNumber array and changed the code a bit

--- javascript/javascript2/week2/doubleOddNumber/oddNumber.js
+++ javascript/javascript2/week2/doubleOddNumber/oddNumber.js
@@ -1,8 +1,10 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-let newNumbers = [];
 
 const doubleOddNumbers = numbers
-  .filter(number => (number % 2 !== 0 ? true : false))
-  .map(number => newNumbers.push(number * 2));
+	.filter(number => (number % 2 !== 0 ? true : false))
+	.map(number => {
+		const newNumber = number * 2;
+		return newNumber;
+	});
 
-console.log("The doubled numbers are", newNumbers);
+console.log(`The doubled numbers are ${doubleOddNumbers}`);
