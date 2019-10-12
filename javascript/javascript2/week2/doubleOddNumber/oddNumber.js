92147e1da4ac2f7e221e20478a4f00459466e11a
Sowmya Mannem <sowmyamannemr@gmail.com>
2019-10-12 17:12:50 +0200

    removed terinary operator on the filter function

--- javascript/javascript2/week2/doubleOddNumber/oddNumber.js
+++ javascript/javascript2/week2/doubleOddNumber/oddNumber.js
@@ -1,7 +1,7 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const doubleOddNumbers = numbers
-	.filter(number => (number % 2 !== 0 ? true : false))
+	.filter(number => number % 2 !== 0)
 	.map(number => {
 		const newNumber = number * 2;
 		return newNumber;
