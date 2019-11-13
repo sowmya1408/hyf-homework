bb178183878f8d06ed02e4b7d9f60cf6ca72c8b9
Sowmya Mannem <sowmyamannemr@gmail.com>
2019-11-13 17:00:40 +0100

    added parameters to the function

--- javascript/javascript3/week3/newCircleClass/newCircleClass.js
+++ javascript/javascript3/week3/newCircleClass/newCircleClass.js
@@ -11,7 +11,6 @@ class Circle {
     draw() {
     const canvas = document.querySelector('canvas');
      const ctx = canvas.getContext('2d');
-
         ctx.beginPath()
         ctx.arc(this.xCordinate, this.yCordinate, this.radius, this.startAngle, this.endAngle)
         ctx.stroke();
@@ -20,21 +19,21 @@ class Circle {
     }
 }
 
-const x = function () {
-   return Math.floor(Math.random() * 1250);
+const x = function (xCordinate) {
+   return Math.floor(Math.random() * xCordinate);
 }
-const y = function() {  
-  return Math.floor(Math.random() * 1250);
+const y = function(yCordinate) {  
+  return Math.floor(Math.random() * yCordinate);
 }  
 
-const radius = function () {
-    return Math.floor(Math.random() * 50);
+const radius = function (r) {
+    return Math.floor(Math.random() * r);
 }
 
-const fillColor = function () {
-    return '#' + Math.floor(Math.random() * (256 * 256 * 256));
+const fillColor = function (color) {
+    return '#' + Math.floor(Math.random() * color);
 }
 setInterval(() => {
-const newCircle = new Circle(x(),y(),radius(), 0, 2 * Math.PI, fillColor());
+const newCircle = new Circle(x(1250),y(1250),radius(50), 0, 2 * Math.PI, fillColor(1250));
 return newCircle.draw(); 
 }, 500)
