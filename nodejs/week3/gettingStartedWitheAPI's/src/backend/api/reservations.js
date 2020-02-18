02f847f8e0b393f832f0295999ddc405a9b9a5c5
Sowmya Mannem <sowmyamannemr@gmail.com>
2020-02-18 22:41:44 +0100

    made changes based on feedback

--- nodejs/week3/gettingStartedWitheAPI's/src/backend/api/reservations.js
+++ nodejs/week3/gettingStartedWitheAPI's/src/backend/api/reservations.js
@@ -10,58 +10,51 @@ reservationsRouter.use(bodyParser.json());
 const { MYSQL_URL } = process.env;
 const connection = mysql.createConnection(MYSQL_URL);
 
-reservationsRouter.get('/', (req, res) => {
-	connection.query('select * from reservation', (err, results, fields) => {
+const reservationsAdd = (queryType, queryParameter) => {
+	connection.query(queryType,queryParameter, (err, results, fields) => {
 		if (err) {
 			return res.send(err);
 		}
 		res.json(results);
 	});
+
+
+}
+
+const reservationsData = (queryType) => {
+	connection.query(queryType, (err, results, fields) => {
+		if (err) {
+			return res.send(err);
+		}
+		res.json(results);
+	})
+}
+
+reservationsRouter.get('/', (req, res) => {
+	reservationsData('select * from reservation')
 });
 
 reservationsRouter.post('/', (req, res) => {
 	const reservation = req.body;
+	reservationsAdd('INSERT into reservation SET ?', reservation)
 
-	connection.query('INSERT into reservation SET ?', reservation, (err, results, fields) => {
-		if (err) {
-			return res.send(err);
-		}
-		res.json(results);
-	});
+
 });
 
 reservationsRouter.get('/:id', (req, res) => {
 	const id = parseInt(req.params.id);
-
-	connection.query(`select * from reservation WHERE id=${id}`, (err, results, fields) => {
-		if (err) {
-			return res.send(err);
-		}
-		res.json(results);
-	});
+	reservationsData(`select * from reservation WHERE id=${id}`)
 });
 
 reservationsRouter.put('/:id', (req, res) => {
 	const reservation = req.body;
 	const id = parseInt(req.params.id);
-
-	connection.query(` UPDATE meal SET ? WHERE id=${id}`, reservation, (err, results, fields) => {
-		if (err) {
-			return res.send(err);
-		}
-		res.json(results);
-	});
+	reservationsAdd(`UPDATE meal SET ? WHERE id=${id}`, reservation)
 });
 
 reservationsRouter.delete('/:id', (req, res) => {
 	const id = parseInt(req.params.id);
-
-	connection.query(` DELETE from meal WHERE id=${id}`, (err, results, fields) => {
-		if (err) {
-			return res.send(err);
-		}
-		res.json(results);
-	});
+	reservationsData(`DELETE from meal WHERE id=${id}`)
 });
 
 module.exports = reservationsRouter;
--- nodejs/week3/gettingStartedWitheAPI's/src/backend/api/review.js
+++ nodejs/week3/gettingStartedWitheAPI's/src/backend/api/review.js
@@ -9,58 +9,50 @@ reviewsRouter.use(bodyParser.json());
 const { MYSQL_URL } = process.env;
 const connection = mysql.createConnection(MYSQL_URL);
 
-reviewsRouter.get('/', (req, res) => {
-	connection.query('select * from review', (err, results, fields) => {
+const reviewsAddOrDelete = (queryType, queryParameter) => {
+	connection.query(queryType,queryParameter, (err, results, fields) => {
 		if (err) {
 			return res.send(err);
 		}
 		res.json(results);
 	});
+
+
+}
+
+const reviewsData = (queryType) => {
+	connection.query(queryType, (err, results, fields) => {
+		if (err) {
+			return res.send(err);
+		}
+		res.json(results);
+	})
+}
+
+reviewsRouter.get('/', (req, res) => {
+	reviewsData('select * from review')
 });
 
 reviewsRouter.post('/', (req, res) => {
 	const review = req.body;
-
-	connection.query('INSERT into review SET ?', review, (err, results, fields) => {
-		if (err) {
-			return res.send(err);
-		}
-		res.json(results);
-	});
+	reviewsAddOrDelete('INSERT into review SET ?', review)
 });
 
 reviewsRouter.get('/:id', (req, res) => {
 	const id = parseInt(req.params.id);
-
-	connection.query(`select * from review WHERE id=${id}`, (err, results, fields) => {
-		if (err) {
-			return res.send(err);
-		}
-		res.json(results);
-	});
+	reviewsData(`select * from review WHERE id=${id}`)
 });
 
 reviewsRouter.put('/:id', (req, res) => {
 	const review = req.body;
 	const id = parseInt(req.params.id);
-
-	connection.query(` UPDATE meal SET ? WHERE id=${id}`, review, (err, results, fields) => {
-		if (err) {
-			return res.send(err);
-		}
-		res.json(results);
-	});
+	reviewsAddOrDelete(` UPDATE meal SET ? WHERE id=${id}`, review)
 });
 
 reviewsRouter.delete('/:id', (req, res) => {
 	const id = parseInt(req.params.id);
+	reviewsAddOrDelete(`DELETE from meal WHERE id=${id}`, review)
 
-	connection.query(` DELETE from meal WHERE id=${id}`, (err, results, fields) => {
-		if (err) {
-			return res.send(err);
-		}
-		res.json(results);
-	});
 });
 
 module.exports = reviewsRouter;
